10d6a79d4b625099153152abb567cd290ae8bac5
10000highfives <jared.a.fowler@gmail.com>
2016-02-03 21:24:33 -0800

    Makes tabFromArray function

--- background.js
+++ background.js
@@ -1,5 +1,12 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: 'console.log(document.title)'
-  });
+  //test run: creates 1 new tab with a search term : bananas
+  code: chrome.tabs.create({ url: "http://www.google.com/#q=bananas", windowId: 1 });
 });
+
+//
+function makeTabsFromArray(arrayOfSearchTermsStrings) {
+  console.log('array at i ', arrayOfSearchTermsStrings[i]);
+  for (var i = 0; i < arrayOfSearchTermsStrings.length; i++) {
+    chrome.tabs.create({ url: "http://www.google.com/#q=" + term, windowId: i + 1 });
+  }
+}
