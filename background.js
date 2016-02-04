697d9ad5a165d005b03df9e70c842c74788587f4
10000highfives <jared.a.fowler@gmail.com>
2016-02-04 13:56:18 -0800

    XHR to custom search, creates a single tab

--- background.js
+++ background.js
@@ -6,9 +6,35 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
 //listen for sendMessage from getHeadings
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log('request from getheadings ', request);
-    console.log('the sender is ', sender);
-    makeTabsFromArray(request.arr)
+    console.log('the first term from getHeadings ', request.arr[0]);
+
+    var cx = '006093038672231679913:0iime5eaerc';
+    //do an AJAX request to our custom search engine and return the url to makeTabs
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://www.googleapis.com/customsearch/v1?&cx=006093038672231679913:0iime5eaerc&key=AIzaSyAqiP2UZ4VHCc_SQHGvCbvie-6SinCFHfA&q=bananas', false);
+    xhr.send();
+
+    if (xhr.status === 200) {
+     //parse responsText to JSON
+     var responseObj = JSON.parse(xhr.responseText);
+     var linkString = responseObj.items[0].link;
+
+     //pass link value to makeTabs
+     makeTab(linkString);
+
+    }
+
+    // xhr.onload = function () {
+    //   if (xhr.readystate === 4) {
+    //     console.log('very gotten on the onload');
+    //     if (xhr.status === 200) {
+    //       console.log('responseText is ', xhr.responseText);
+    //     } else {
+    //       console.error('errorzz ', xhr.responseText);
+    //     }
+    //   }
+
   });
 
 
@@ -17,4 +43,10 @@ function makeTabsFromArray(arrayOfSearchTermsStrings) {
   for (var i = 0; i < arrayOfSearchTermsStrings.length; i++) {
     chrome.tabs.create({ url: "http://www.google.com/#q=" + arrayOfSearchTermsStrings[i], windowId: 1 });
   }
+}
+
+
+//make a single tab from link string (for testing)
+function makeTab(linkString) {
+    chrome.tabs.create({ url: linkString});
 }
