c24cedb10cba4f12a868ca00daa158f06685bb4a
10000highfives <jared.a.fowler@gmail.com>
2016-02-04 00:26:12 -0800

    Code clean up

--- background.js
+++ background.js
@@ -1,10 +1,7 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.executeScript(null, { file: 'getHeadings.js' });
-
-  //makeTabsFromArray(getHeadings());
   }
 );
-
 
 //listen for sendMessage from getHeadings
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -17,7 +14,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 //take return from getHeadings.js and generate tabs from it.
 function makeTabsFromArray(arrayOfSearchTermsStrings) {
   for (var i = 0; i < arrayOfSearchTermsStrings.length; i++) {
-		//console.log('array at i ', arrayOfSearchTermsStrings[i]);
-    chrome.tabs.create({ url: "http://www.google.com/#q=" + arrayOfSearchTermsStrings[i], windowId: i + 1 });
+    chrome.tabs.create({ url: "http://www.google.com/#q=" + arrayOfSearchTermsStrings[i], windowId: 1 });
   }
 }
