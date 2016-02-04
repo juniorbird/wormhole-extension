ced18a28e1af35356076cc920a4ba80cc2ec9ee0
Wade Armstrong <wade@wadearmstrong.com>
2016-02-03 21:30:07 -0800

    reusable function gets array of headings on page when button is clicked

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript({
-    code: 'console.log(document.title)'
-  });
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.executeScript(null, { file: 'getHeadings.js' });
 });
