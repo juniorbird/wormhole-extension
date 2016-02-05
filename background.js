55970e531755ac63056a143bc035aaa7f7326a68
10000highfives <jared.a.fowler@gmail.com>
2016-02-04 16:27:34 -0800

    Completes async XMLHttpRequest to first 5 search terms; creates 5 new tabs from the first search result URLs.

--- background.js
+++ background.js
@@ -3,34 +3,35 @@ chrome.browserAction.onClicked.addListener(function (tab) {
   }
 );
 
-//Listens for Array of search terms from getHeadings script. 
+//Listens for Array of search terms from getHeadings script.
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     //for each search term, do an AJAX request to our custom search engine
     //parse the responseText, grab the link at items and make a new tab at that URL
     request.arr.forEach(function (searchTerm) {
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://www.googleapis.com/customsearch/v1?&cx=006093038672231679913:0iime5eaerc&key=AIzaSyAqiP2UZ4VHCc_SQHGvCbvie-6SinCFHfA&q=' + searchTerm, false);
+      xhr.onreadystatechange = parseResponseAndMakeTab;
+      xhr.open('GET', 'https://www.googleapis.com/customsearch/v1?&cx=006093038672231679913:0iime5eaerc&key=AIzaSyAqiP2UZ4VHCc_SQHGvCbvie-6SinCFHfA&q=' + searchTerm, true);
       xhr.send();
-      if (xhr.status === 200) {
-        //parse responsText to JSON
-        var responseObj = JSON.parse(xhr.responseText);
-        var linkString = responseObj.items[0].link;
 
-        //pass link value to makeTabs
-        makeTab(linkString);
+      function parseResponseAndMakeTab () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+          if (xhr.status === 200) {
+            //parse responseText string
+            var responseObj = JSON.parse(xhr.responseText);
+
+            //grab first URL in the items array. TODO: make helper fn to get random link from item;
+            var linkString = responseObj.items[0].link;
+
+            makeTab(linkString);
+          } else {
+            console.error('Error: cannot GET ' + xhr.status);
+          }
+        }
       }
     });
 });
 
-//take return from getHeadings.js and generate tabs from it.
-function makeTabsFromArray(arrayOfSearchTermsStrings) {
-  for (var i = 0; i < arrayOfSearchTermsStrings.length; i++) {
-    chrome.tabs.create({ url: "http://www.google.com/#q=" + arrayOfSearchTermsStrings[i], windowId: 1 });
-  }
-}
-
-
-//make a single tab from link string (for testing)
+//make a single tab from link string
 function makeTab(linkString) {
     chrome.tabs.create({ url: linkString});
 }
